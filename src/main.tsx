46b6059713bc7ab721370dfc2ed87d778bb9cadd
lizunlong <lizunlong@gmail.com>
2024-03-11 20:50:13 +0800

    feat: use react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { Link, RouterProvider, createBrowserRouter } from 'react-router-dom';
+// import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div><Link to="/about">Go to About</Link></div>,
+  },
+  {
+    path: "/about",
+    element: <div><Link to="/">Back to Home</Link></div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
