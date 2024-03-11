a17338971db3f2c407b3b64dcb5009838ca7edce
lizunlong <lizunlong@gmail.com>
2024-03-11 22:07:27 +0800

    feat: login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-// import './index.css'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import LoginPage from './routes/login'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter basename="/mstkj-front-end/">
       <Routes>
-      <Route path="/" element={<Link to="/about">Go to About</Link>} />
-      <Route path="/about" element={<Link to="/">Back to Home</Link>} />
+        <Route path="/" element={<Link to="/login">登录</Link>} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
