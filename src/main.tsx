913a7511c59f9601a6401fe7c14bb5ab10309f05
lizunlong <lizunlong@gmail.com>
2024-03-11 21:23:31 +0800

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/mstkj-front-end">
+    <BrowserRouter basename="/mstkj-front-end/">
       <Routes>
       <Route path="/" element={<Link to="/about">Go to About</Link>} />
       <Route path="/about" element={<Link to="/">Back to Home</Link>} />
